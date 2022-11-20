196442324541a20bdae89c87608767508b7567b5
Rafael Macedo Vitor <117114737+rarfel@users.noreply.github.com>
2022-11-19 22:21:55 -0300

    Add files via upload

---
+++ ModernistClock/script.js
@@ -0,0 +1,12 @@
+let tempo = new Date();
+let hora = tempo.getHours();
+let minuto = tempo.getMinutes();
+
+const onze = document.querySelectorAll('[data-onze]')
+const uma = document.querySelectorAll('[data-uma]')
+const cinco = document.querySelectorAll('[data-cinco]')
+const quatro = document.querySelectorAll('[data-quatro]')
+const duas = document.querySelectorAll('[data-duas]')
+const seis = document.querySelectorAll('[data-seis]')
+const tres = document.querySelectorAll('[data-tres]')
+const nove = document.querySelectorAll('[data-nove]')
