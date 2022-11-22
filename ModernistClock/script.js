b78da7a46235e26d8c9470a4c0b41f023bb82894
Rafael Macedo Vitor <117114737+rarfel@users.noreply.github.com>
2022-11-22 07:59:16 -0300

    rework the times space between minutes

--- ModernistClock/script.js
+++ ModernistClock/script.js
@@ -132,92 +132,92 @@ function relogio(){
         case '1':
         case '2':
         case '3':
-        case '4':
             fabrica(horas,'on')
             break;
+        case '4':
         case '5':
         case '6':
         case '7':
-        case '8':
-        case '9':
             fabrica(cincoM,'on')
             fabrica(e,'on')
             break;
+        case '8':
+        case '9':
         case '10':
         case '11':
         case '12':
-        case '13':
-        case '14':
             fabrica(dezM,'on')
             fabrica(e,'on')
             break;
+        case '13':
+        case '14':
         case '15':
         case '16':
         case '17':
-        case '18':
-        case '19':
             fabrica(quinze,'on')
             fabrica(e,'on')
             break;
+        case '18':
+        case '19':
         case '20':
         case '21':
         case '22':
-        case '23':
-        case '24':
             fabrica(vinte,'on')
             fabrica(e,'on')
             break;
+        case '23':
+        case '24':
         case '25':
         case '26':
         case '27':
-        case '28':
-        case '29':
             fabrica(vinte,'on')
             fabrica(cincoM,'on')
             fabrica(e,'on')
             break;
+        case '28':
+        case '29':
         case '30':
         case '31':
         case '32':
-        case '33':
-        case '34':
             fabrica(meia,'on')
             fabrica(e,'on')
             break;
+        case '33':
+        case '34':
         case '35':
         case '36':
         case '37':
-        case '38':
-        case '39':
             fabrica(trinta,'on')
             fabrica(cincoM,'on')
             fabrica(e,'on')
             break;
+        case '38':
+        case '39':
         case '40':
         case '41':
         case '42':
-        case '43':
-        case '44':
             fabrica(quarenta,'on')
             fabrica(e,'on')
             break;
+        case '43':
+        case '44':
         case '45':
         case '46':
         case '47':
-        case '48':
-        case '49':
             fabrica(quarenta,'on')
             fabrica(cincoM,'on')
             fabrica(e,'on')
             break;
+        case '48':
+        case '49':
         case '50':
         case '51':
         case '52':
-        case '53':
-        case '54':
             fabrica(cinquenta,'on')
             fabrica(e,'on')
             break;
+        case '53':
+        case '54':
         case '55':
         case '56':
         case '57':
